250d24a6db42978e0aeadaccf72d11944f696511
Astgh78 <astghik.karakhanyan@gmail.com>
2020-08-17 18:01:05 +0400

    - added readme file - added project files - made some changes but still on working stage - did some changes - did some updates

--- Person/src/com.company/core/model/BasePerson.java
+++ Person/src/com.company/core/model/BasePerson.java
@@ -1,6 +1,6 @@
 package Core.model;
 
-import Core.exceptions.InvalidLastNameException;
+import Core.exceptions.WrongInputException;
 import com.company.core.service.PersonAction;
 
 import javax.naming.InvalidNameException;
@@ -26,42 +26,34 @@ public abstract class BasePerson implements PersonAction {
         this.age = age;
     }
 
-    // creating getters
+    // creating setters
 
     public String setName() {
         try {
             if (0 < name.length() && name.length() < 255)
                 return name;
             else {
-                throw new InvalidNameException();
-            }
-        } catch (InvalidNameException e) {
-            e.printStackTrace();
-        }
-
-
-    public String setLastName() {
-            try {
-                if (0 < lastName.length() && lastName.length() < 255)
-                    return lastName;
-                else {
-                    throw new InvalidLastNameException();
-                }
-            } catch (InvalidLastNameException e) {
-                e.printStackTrace();
+                throw new WrongInputException("Invalid name");
             }
         }
+    }
+
+            public String setLastName() {
+                try {
+                    if (0 < lastName.length() && lastName.length() < 255)
+                        return lastName;
+                    else {
+                        throw new WrongInputException("Invalid lastName");
+                    }
 
     public String setNickname() {
             try {
                 if (0 < nickName.length() && nickName.length() < 255)
-                    return name;
+                    return nickName;
                 else{
-
+                    throw new WrongInputException("Invalid nickName");
                 }
-        }
-        return nickname;
-    }
+           }
 
     public String getEmail() {
         return email;
